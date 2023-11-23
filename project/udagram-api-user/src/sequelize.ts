d3cc7b647e63c3195cc365aee1219b83f39a16cd
Sebastian Tleye <stleye@gmail.com>
2023-11-23 12:20:23 -0300

    Started With Refactor

---
+++ project/udagram-api-user/src/sequelize.ts
@@ -0,0 +1,15 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+
+  'dialect': config.dialect,
+  'storage': ':memory:',
+
+  'dialectOptions': config.dialectOptions,
+});
