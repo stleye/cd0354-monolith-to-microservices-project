fd9cb6c9ef3d3f2c34b72af834ab7ecdca4934b8
Sebastian Tleye <stleye@gmail.com>
2023-12-03 16:13:53 -0300

    Added configuration files

--- project/udagram-frontend/src/environments/environment.prod.ts
+++ project/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a58455495f38f4557899457c66011202-712704585.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
